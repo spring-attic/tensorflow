5b61bbe11e689d10919ea2313df826b3a87e4b8d
Christian Tzolov <christian.tzolov@gmail.com>
2018-04-25 15:59:53 +0200

    Add Image Instance Segmentation Support
    Resolves #10
    
    - upgrade to TF 1.8.0
    - Improve javadoc

--- spring-cloud-starter-stream-processor-object-detection/src/test/java/org/springframework/cloud/stream/app/object/detection/processor/detection/ObjectDetectionTensorflowProcessorIntegrationTests.java
+++ spring-cloud-starter-stream-processor-object-detection/src/test/java/org/springframework/cloud/stream/app/object/detection/processor/detection/ObjectDetectionTensorflowProcessorIntegrationTests.java
@@ -53,6 +53,7 @@ import static org.hamcrest.Matchers.equalTo;
 				//"tensorflow.modelFetch=detection_scores,detection_classes,detection_boxes,detection_masks,num_detections",
 				"tensorflow.modelFetch=detection_scores,detection_classes,detection_boxes,num_detections",
 				"tensorflow.model=http://dl.bintray.com/big-data/generic/faster_rcnn_resnet101_coco_2018_01_28_frozen_inference_graph.pb",
+				//"tensorflow.model=file:/Users/ctzolov/Downloads/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb",
 				"tensorflow.object.detection.labels=http://dl.bintray.com/big-data/generic/mscoco_label_map.pbtxt",
 				//"tensorflow.model=file:/Users/ctzolov/Downloads/mask_rcnn_resnet101_atrous_coco_2018_01_28/frozen_inference_graph.pb"
 				//"tensorflow.model=file:/Users/ctzolov/Downloads/ssd_inception_v2_coco_2017_11_17/frozen_inference_graph.pb",
@@ -68,7 +69,7 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 	@Autowired
 	protected MessageCollector messageCollector;
 
-	@Ignore("Exclude the Processor Integration Test until a proper Mock TF Model is provided!")
+	//	@Ignore("Exclude the Processor Integration Test until a proper Mock TF Model is provided!")
 	@TestPropertySource(properties = {
 			"tensorflow.mode=header"
 	})
@@ -76,23 +77,31 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 
 		@Test
 		public void testEvaluationPositive() throws IOException {
-			try (InputStream is = new ClassPathResource("/images/tourists.jpg").getInputStream()) {
+			//try (InputStream is = new ClassPathResource("/images/tourists.jpg").getInputStream()) {
+			try (InputStream is = new ClassPathResource("/images/object-detection.jpg").getInputStream()) {
 
 				byte[] image = StreamUtils.copyToByteArray(is);
 
 				testEvaluationWithOutputInHeader(
-						image,
-						"{\"labels\":[" +
-								"{\"person\":0.9996774,\"x1\":0.0,\"y1\":0.3940161,\"x2\":0.9465165,\"y2\":0.5592592,\"cid\":1}," +
-								"{\"person\":0.9996604,\"x1\":0.047891676,\"y1\":0.03169123,\"x2\":0.941098,\"y2\":0.2085562,\"cid\":1}," +
-								"{\"person\":0.9994741,\"x1\":0.038780525,\"y1\":0.20172822,\"x2\":0.9464298,\"y2\":0.39004815,\"cid\":1}," +
-								"{\"person\":0.99946576,\"x1\":0.0632496,\"y1\":0.79281414,\"x2\":0.96308815,\"y2\":0.99012446,\"cid\":1}," +
-								"{\"person\":0.99867463,\"x1\":0.051441744,\"y1\":0.57940257,\"x2\":0.9477767,\"y2\":0.8259268,\"cid\":1}," +
-								"{\"backpack\":0.96534747,\"x1\":0.15588468,\"y1\":0.85957795,\"x2\":0.5091308,\"y2\":0.9908878,\"cid\":27}," +
-								"{\"backpack\":0.963343,\"x1\":0.1273736,\"y1\":0.57658505,\"x2\":0.47765,\"y2\":0.6986431,\"cid\":27}," +
-								"{\"backpack\":0.9294457,\"x1\":0.14560387,\"y1\":0.022079457,\"x2\":0.53702116,\"y2\":0.113561034,\"cid\":27}," +
-								"{\"backpack\":0.6643621,\"x1\":0.53017575,\"y1\":0.31962925,\"x2\":0.77074707,\"y2\":0.4030401,\"cid\":27}," +
-								"{\"backpack\":0.57413465,\"x1\":0.1278874,\"y1\":0.457999,\"x2\":0.3998378,\"y2\":0.5622766,\"cid\":27}]}");
+						image, "{\"labels\":[" +
+								"{\"person\":0.99666023,\"x1\":0.7767177,\"y1\":0.15963306,\"x2\":0.9540965,\"y2\":0.20158374,\"cid\":1}," +
+								"{\"kite\":0.99346715,\"x1\":0.08797318,\"y1\":0.44025636,\"x2\":0.16985515,\"y2\":0.49672586,\"cid\":38}," +
+								"{\"person\":0.99229527,\"x1\":0.6840088,\"y1\":0.08519748,\"x2\":0.843071,\"y2\":0.12301033,\"cid\":1}," +
+								"{\"person\":0.9789544,\"x1\":0.56879956,\"y1\":0.06220094,\"x2\":0.629103,\"y2\":0.07995274,\"cid\":1}," +
+								"{\"kite\":0.96934885,\"x1\":0.26183245,\"y1\":0.2070512,\"x2\":0.3146273,\"y2\":0.22642946,\"cid\":38}," +
+								"{\"kite\":0.9462258,\"x1\":0.43824652,\"y1\":0.8026538,\"x2\":0.4702088,\"y2\":0.8175442,\"cid\":38}," +
+								"{\"kite\":0.92378986,\"x1\":0.37467834,\"y1\":0.3474477,\"x2\":0.3987156,\"y2\":0.36179984,\"cid\":38}," +
+								"{\"kite\":0.9130423,\"x1\":0.38156357,\"y1\":0.42770827,\"x2\":0.41003278,\"y2\":0.4449141,\"cid\":38}," +
+								"{\"kite\":0.89646775,\"x1\":0.41558802,\"y1\":0.22641928,\"x2\":0.45246547,\"y2\":0.24253754,\"cid\":38}," +
+								"{\"person\":0.83506095,\"x1\":0.601887,\"y1\":0.13212581,\"x2\":0.63641346,\"y2\":0.14473228,\"cid\":1}," +
+								"{\"person\":0.7343776,\"x1\":0.5767689,\"y1\":0.023850055,\"x2\":0.62417924,\"y2\":0.038195316,\"cid\":1}," +
+								"{\"person\":0.58631957,\"x1\":0.5590419,\"y1\":0.38939288,\"x2\":0.5883101,\"y2\":0.3997791,\"cid\":1}," +
+								"{\"person\":0.57765806,\"x1\":0.5586649,\"y1\":0.38527238,\"x2\":0.58879423,\"y2\":0.3959817,\"cid\":1}," +
+								"{\"person\":0.572748,\"x1\":0.5611651,\"y1\":0.3912078,\"x2\":0.59040546,\"y2\":0.4047312,\"cid\":1}," +
+								"{\"person\":0.55604917,\"x1\":0.54020363,\"y1\":0.25858936,\"x2\":0.5598617,\"y2\":0.2650398,\"cid\":1}," +
+								"{\"kite\":0.46935454,\"x1\":0.4220001,\"y1\":0.5656192,\"x2\":0.43472755,\"y2\":0.57504195,\"cid\":38}," +
+								"{\"person\":0.43634608,\"x1\":0.5645142,\"y1\":0.3955184,\"x2\":0.5911028,\"y2\":0.4101856,\"cid\":1}]}"
+				);
 			}
 		}
 
@@ -103,7 +112,7 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 		}
 	}
 
-	@Ignore("Exclude the Processor Integration Test until a proper Mock TF Model is provided!")
+	//	@Ignore("Exclude the Processor Integration Test until a proper Mock TF Model is provided!")
 	@TestPropertySource(properties = {
 			"tensorflow.mode=payload"
 	})
