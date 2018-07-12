effa51b257ca14682c9afbdaebe9889d325a5d95
Christian Tzolov <christian.tzolov@gmail.com>
2018-07-12 10:06:33 +0200

    Use test json files to address review comments

--- spring-cloud-starter-stream-processor-object-detection/src/test/java/org/springframework/cloud/stream/app/object/detection/processor/detection/ObjectDetectionTensorflowProcessorIntegrationTests.java
+++ spring-cloud-starter-stream-processor-object-detection/src/test/java/org/springframework/cloud/stream/app/object/detection/processor/detection/ObjectDetectionTensorflowProcessorIntegrationTests.java
@@ -19,15 +19,18 @@ package org.springframework.cloud.stream.app.object.detection.processor.detectio
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.Assert;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.app.object.detection.processor.ObjectDetectionProcessorConfiguration;
+import org.springframework.cloud.stream.app.test.tensorflow.JsonUtils;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.context.annotation.Import;
@@ -38,8 +41,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StreamUtils;
-
-import static org.hamcrest.Matchers.equalTo;
 
 /**
  * @author Christian Tzolov
@@ -76,21 +77,17 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 	public static class OutputInHeaderTests extends ObjectDetectionTensorflowProcessorIntegrationTests {
 
 		@Test
-		public void testEvaluationPositive() throws IOException {
+		public void testHeaderMode() throws IOException, JSONException {
 			try (InputStream is = new ClassPathResource("/images/object-detection.jpg").getInputStream()) {
 
 				byte[] image = StreamUtils.copyToByteArray(is);
 
-				testEvaluationWithOutputInHeader(
-						image, "{\"labels\":[{\"kite\":0.8673682,\"x1\":0.0814952,\"y1\":0.44308642,\"x2\":0.169772,\"y2\":0.5014239,\"cid\":38},{\"kite\":0.80015683,\"x1\":0.37845963,\"y1\":0.34496674,\"x2\":0.4024166,\"y2\":0.3610665,\"cid\":38},{\"person\":0.7876431,\"x1\":0.5630007,\"y1\":0.3913834,\"x2\":0.59502745,\"y2\":0.4083495,\"cid\":1},{\"person\":0.7242934,\"x1\":0.68023187,\"y1\":0.08181207,\"x2\":0.83192676,\"y2\":0.124822214,\"cid\":1},{\"person\":0.6290599,\"x1\":0.57875264,\"y1\":0.059143387,\"x2\":0.61880136,\"y2\":0.07551455,\"cid\":1},{\"person\":0.6122513,\"x1\":0.5782344,\"y1\":0.0257214,\"x2\":0.6188131,\"y2\":0.04140708,\"cid\":1},{\"kite\":0.607728,\"x1\":0.27496633,\"y1\":0.20563951,\"x2\":0.3100944,\"y2\":0.22761866,\"cid\":38},{\"person\":0.53252214,\"x1\":0.76527464,\"y1\":0.15765251,\"x2\":0.9485351,\"y2\":0.20344453,\"cid\":1}]}"
-				);
-			}
-		}
+				channels.input().send(MessageBuilder.withPayload(image).build());
+				Message<?> received = messageCollector.forChannel(channels.output()).poll();
 
-		private void testEvaluationWithOutputInHeader(byte[] image, String resultJson) {
-			channels.input().send(MessageBuilder.withPayload(image).build());
-			Message<?> received = messageCollector.forChannel(channels.output()).poll();
-			Assert.assertThat(received.getHeaders().get("result").toString(), equalTo(resultJson));
+				JSONObject expected = new JSONObject(JsonUtils.resourceToString("classpath:/test-object-detection.json"));
+				JSONAssert.assertEquals(expected, new JSONObject(new String(received.getHeaders().get("result").toString())), false);
+			}
 		}
 	}
 
@@ -101,7 +98,7 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 	public static class OutputInPayloadTests extends ObjectDetectionTensorflowProcessorIntegrationTests {
 
 		@Test
-		public void testEvaluationPositive() throws IOException {
+		public void testPayloadMode() throws IOException, JSONException {
 			try (InputStream is = new ClassPathResource("/images/panda.jpeg").getInputStream()) {
 
 				byte[] image = StreamUtils.copyToByteArray(is);
@@ -110,8 +107,8 @@ public abstract class ObjectDetectionTensorflowProcessorIntegrationTests {
 
 				Message<byte[]> received = (Message<byte[]>) messageCollector.forChannel(channels.output()).poll();
 
-				Assert.assertThat(new String(received.getPayload()),
-						equalTo("{\"labels\":[{\"bear\":0.9912719,\"x1\":0.082054436,\"y1\":0.31035388,\"x2\":0.8720296,\"y2\":0.7736771,\"cid\":23}]}"));
+				JSONObject expected = new JSONObject(JsonUtils.resourceToString("classpath:/test-panda.json"));
+				JSONAssert.assertEquals(expected, new JSONObject(new String(received.getPayload())), false);
 			}
 		}
 	}
